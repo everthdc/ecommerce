a08b35ecc2fabc64fd72413f5c63b6b79466bbc9
everthdc <145787218+everthdc@users.noreply.github.com>
2024-03-21 02:13:53 -0500

    Agregar middleware a archivo de rutas admin y habilitar fotos de perfil de jetstream

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         //Archivos de rutas personalizadas
         then: function () {
-            Route::middleware('web')
+            Route::middleware('web', 'auth')
                 ->prefix('admin')
                 ->name('admin.')
                 ->group(base_path('routes/admin.php'));
